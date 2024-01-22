a43efc1c96fbd8eb046e29e30837efcecefa2b97
Connor <connor_email@yahoo.com>
2024-01-22 02:29:34 -0500

    commited fix

--- src/index.js
+++ src/index.js
@@ -1,59 +1,10 @@
 import React from "react";
-import { useFormik } from "formik";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-function App() {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    onSubmit: (values) => {
-      alert("Login Successful");
-    },
-    validate: (values) => {
-      let errors = {};
-      if (!values.email) errors.email = "field required";
-      if (!values.password) errors.password = "field required";
-      return errors;
-    },
-  });
-
-  return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
-        <div>Email:</div>
-        <input
-          id="emailField"
-          type="text"
-          name="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-        />
-        {formik.errors.email ? (
-          <div id="emailError" style={{ color: "red" }}>
-            {formik.errors.email}
-          </div>
-        ) : null}
-        <div>Password:</div>
-        <input
-          id="pswField"
-          type="text"
-          name="password"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-        />
-        <br />
-        {formik.errors.password ? (
-          <div id="pswError" style={{ color: "red" }}>
-            {formik.errors.password}
-          </div>
-        ) : null}
-        <button id="submitBtn" type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
